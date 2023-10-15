01016174f425e50b21da4481019ef6fb4870a0dc
sylee <maestro30232@gmail.com>
2023-10-15 16:40:55 +0900

    누락 수정

--- src/main/java/com/dmuIt/domain/service/MessageService.java
+++ src/main/java/com/dmuIt/domain/service/MessageService.java
@@ -55,7 +55,7 @@ public class MessageService {
                 .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
 
         Community community = communityRepository.findById(comId)
-                .orElseThrow(() -> new IllegalArgumentException("게시글을 찾을 수 없습니다."));
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.COMMUNITY_NOT_FOUND));
         ;
 
         Member receiver = community.getMember();
@@ -172,7 +172,7 @@ public class MessageService {
     @Transactional
     public Object deleteMessage(long id, Member member) {
         Message message = messageRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("메시지를 찾을 수 없습니다."));
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MESSAGE_NOT_FOUND));
         List<Message> messages = messageRepository.findByRoom(message.getRoom().getRoom());
 
         if(member.getNickname().equals(message.getSenderName())) // 내가 보낸 메세지 삭제
